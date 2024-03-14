2d5fe7ed8e3c100ea1501f35730490cc70b7d799
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-03-14 20:33:48 +0100

    Made navbar responsive with mobile menu

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,9 +1,13 @@
 import { Navigation }  from "../../Navigation";
+import { MobileMenu } from "../../MobileMenu";
+import useMediaQuery from "../../../hooks/useMediaQuery";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 import { CartIcon } from "../../CartIcon";
 
 export const Header = () => {
+    const isMobile = useMediaQuery('(max-width: 768px)');
+    
     return (
         <>
             <header className="header__container">
@@ -11,10 +15,10 @@ export const Header = () => {
                     <div className="logo">HEYA</div>
                 </Link>
                 <div className="nav__container">
-                <Navigation />
-                <CartIcon />
+                    {isMobile ? <MobileMenu /> : <Navigation />}    
+                    <CartIcon />
                 </div>
             </header>
         </>
-    )
-}
+    );
+};
--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -1,29 +1,30 @@
 import { Link } from "react-router-dom";
-import { useState} from "react";
+import { useState } from "react";
+import "./MobileMenu.scss";
 
 export const MobileMenu = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    }
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div>
-      <button oncClick={toggleMenu}>
-        Menu
-      </button>
-      <nav>
-        <menu>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </menu>
-      </nav>
+    <div className="menu__container">
+      <button onClick={toggleMenu}>Menu</button>
+      {open && (
+        <nav>
+          <menu>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </menu>
+        </nav>
+      )}
     </div>
   );
 };
