992b15616997e8d8ad0b7af23cce06e5c8a1d36e
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-03-23 11:48:24 +0100

    Success page finished

--- src/pages/Success/index.jsx
+++ src/pages/Success/index.jsx
@@ -1,12 +1,14 @@
 import "./Success.scss";
 import { Button } from "../../components";
+import confirmImg from "../../assets/images/confirm.png";
 
 export const Success = () =>  {
     return (
         <div className="success__page__container">
+            <img src={confirmImg} alt="Confirmed" />
             <h1>Success!</h1>
             <p>Your purchase is on the way.</p>
-            <Button to="/">Back to shop</Button>
+            <Button to="/" variant="black">Back to shop</Button>
         </div>
     )
 }
